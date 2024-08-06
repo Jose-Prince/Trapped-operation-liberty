5b72700890d7c3433a7d4a69d2650105f7829064
Jose-Prince <joseprince148@gmail.com>
2024-08-05 22:20:27 -0600

    Feat: WASD movement

--- src/player.rs
+++ src/player.rs
@@ -24,35 +24,58 @@ impl Player {
     pub fn process_events(&mut self, window: &Window, maze: &Vec<Vec<char>>, block_size: usize, framebuffer: &mut Framebuffer) {
         const MOVE_SPEED: f32 = 5.0;
         const ROTATION_SPEED: f32 = std::f32::consts::PI / 30.0;
-
+    
         let cos_a = self.a.cos();
         let sin_a = self.a.sin();
-
+    
+        // Rotación de la cámara con las teclas Left y Right
         if window.is_key_down(Key::Left) {
             self.a -= ROTATION_SPEED;
         }
         if window.is_key_down(Key::Right) {
             self.a += ROTATION_SPEED;
         }
-        if window.is_key_down(Key::Up) {
+    
+        // Movimiento hacia adelante y hacia atrás (W y S)
+        if window.is_key_down(Key::Up) || window.is_key_down(Key::W) {
             let move_x = MOVE_SPEED * cos_a;
             let move_y = MOVE_SPEED * sin_a;
             let new_pos = Vec2::new(self.pos.x + move_x, self.pos.y + move_y);
-
+    
             if !is_wall(maze, (new_pos.x / block_size as f32) as usize, (new_pos.y / block_size as f32) as usize) {
                 self.pos = new_pos;
             }
         }
-        if window.is_key_down(Key::Down) {
+        if window.is_key_down(Key::Down) || window.is_key_down(Key::S) {
             let move_x = -MOVE_SPEED * cos_a;
             let move_y = -MOVE_SPEED * sin_a;
             let new_pos = Vec2::new(self.pos.x + move_x, self.pos.y + move_y);
-
+    
+            if !is_wall(maze, (new_pos.x / block_size as f32) as usize, (new_pos.y / block_size as f32) as usize) {
+                self.pos = new_pos;
+            }
+        }
+    
+        if window.is_key_down(Key::A) {
+            let move_x = MOVE_SPEED * sin_a;
+            let move_y = -MOVE_SPEED * cos_a;
+            let new_pos = Vec2::new(self.pos.x + move_x, self.pos.y + move_y);
+    
+            if !is_wall(maze, (new_pos.x / block_size as f32) as usize, (new_pos.y / block_size as f32) as usize) {
+                self.pos = new_pos;
+            }
+        }
+        if window.is_key_down(Key::D) {
+            let move_x = -MOVE_SPEED * sin_a;
+            let move_y = MOVE_SPEED * cos_a;
+            let new_pos = Vec2::new(self.pos.x + move_x, self.pos.y + move_y);
+    
             if !is_wall(maze, (new_pos.x / block_size as f32) as usize, (new_pos.y / block_size as f32) as usize) {
                 self.pos = new_pos;
             }
         }
     }
+    
 
     pub fn draw(&self, framebuffer: &mut Framebuffer) {
         const PLAYER_SIZE: usize = 5;
