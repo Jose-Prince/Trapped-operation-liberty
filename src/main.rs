04449f81a610b45dd880d5d5ec44519cdbe778b2
Jose-Prince <joseprince148@gmail.com>
2024-08-17 00:06:16 -0600

    Feat: Text blink in home page

--- src/main.rs
+++ src/main.rs
@@ -50,13 +50,26 @@ fn main() {
     });
 
     let begin_page = "textures/Inicio.png";
+
+    let blink_interval = Duration::from_millis(200);
+    let mut last_blink_time = Instant::now();
+    let mut show_text = true;
     
     while window.is_open() && !window.is_key_down(minifb::Key::Enter) && !window.is_key_down(minifb::Key::Escape) {
         framebuffer.clear();
         framebuffer.draw_image(&begin_page, width, height);
-        framebuffer.draw_text(width/2 - 50,height/2,"Press ENTER to start game", Color::new(255,255,255));
+
+        if last_blink_time.elapsed() >= blink_interval {
+            show_text = !show_text;
+            last_blink_time = Instant::now();
+        }
+
+        if show_text {
+            framebuffer.draw_text(width/2 - 50,height/2,"Press ENTER to start game", Color::new(255,255,255));
+        }
 
         window.update_with_buffer(&framebuffer.get_buffer(), width, height).unwrap();
+        std::thread::sleep(Duration::from_millis(16));
     }
 
     audio.stop();
